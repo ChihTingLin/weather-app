c36dd4fafb1ad471ba53b97bcdadd6efdb693f9b
Chih-Ting Lin <chihtinglin03@gmail.com>
2025-03-07 16:25:51 +0800

    feat: add weekly weather data table

--- app/weather/weekly/page.tsx
+++ app/weather/weekly/page.tsx
@@ -3,6 +3,27 @@ import { locations } from "@/app/staticData";
 import Chart from "@/components/Chart";
 import { useEffect, useMemo, useState } from "react";
 import dayjs from "dayjs";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+type Location = {
+  LocationName: string;
+  Geocode: string;
+  Latitude: string;
+  Longitude: string;
+  WeatherElement: [];
+};
+
+type CityLocation = {
+  DatasetDescription: string;
+  LocationName: string;
+  Dataid: string;
+  Location: Location[];
+};
 
 const getChartsOption = (weatherElement: any) => {
   return weatherElement.reduce(
@@ -45,16 +66,84 @@ const getChartsOption = (weatherElement: any) => {
     }
   );
 };
+
+const getTableData = (loc: CityLocation) => {
+  const newLoc: { table: { [key: string]: string }[] } = {
+    ...loc,
+    table: [],
+  };
+  loc.Location.forEach((l: any) => {
+    const row: { location: string; [key: string]: string } = {
+      location: l.LocationName,
+    };
+    l.WeatherElement[1].Time.forEach(
+      (
+        t: {
+          StartTime: string;
+          EndTime: string;
+          ElementValue: {
+            MinTemperature?: string;
+            MaxTemperature?: string;
+          }[];
+        },
+        idx: number
+      ) => {
+        const minTemperature = l.WeatherElement[2].Time[idx];
+        const start = dayjs(t.StartTime);
+        const day_night = start.hour() <= 12 ? "白天" : "晚上";
+        const key = `${start.format("MM/DD")}_${
+          day_night === "白天" ? "am" : "pm"
+        }`;
+        const value = `${minTemperature.ElementValue?.[0]?.MinTemperature} ~ ${t.ElementValue?.[0]?.MaxTemperature}`;
+        row[key] = value;
+      }
+    );
+    newLoc.table.push(row);
+  });
+  return newLoc;
+};
+const columnHelper = createColumnHelper();
+const getTableColumns = (weatherElement: any) => {
+  const dataCols = weatherElement.Time.map((t: any) => {
+    const start = dayjs(t.StartTime);
+    const day_night = start.hour() <= 12 ? "白天" : "晚上";
+    return columnHelper.accessor(
+      `${start.format("MM/DD")}_${day_night === "白天" ? "am" : "pm"}`,
+      {
+        header: `${start.format("MM/DD")} ${day_night}`,
+        id: `${start.format("MM/DD")}_${day_night === "白天" ? "am" : "pm"}`,
+        cell: (i) => <span className="whitespace-nowrap">{i.getValue()}</span>,
+      }
+    );
+  });
+  return [
+    columnHelper.accessor("location", {
+      header: "地點",
+      id: "location",
+      cell: (i) => <div className="whitespace-nowrap">{i.getValue()}</div>,
+    }),
+    ...dataCols,
+  ];
+};
+
 export default function WeeklyWeather() {
   const [location, setLocation] = useState(locations[0]);
   const [distIndex, setDistIndex] = useState<number>(-1);
   const [data, setData] = useState<any>(null);
+  const [columns, setColumns] = useState<any | null>(null);
+  const tableData = useMemo(() => (data ? data.table : []), [data]);
   const dists = useMemo(() => {
     return data?.Location?.map((l: any) => ({
       value: l.LocationName,
       label: l.LocationName,
     }));
   }, [data]);
+
+  const table = useReactTable({
+    data: tableData,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   useEffect(() => {
     fetch(`/api/weather/weekly?locationName=${location}`)
@@ -63,7 +152,9 @@ export default function WeeklyWeather() {
         json.chart = json.Location.map((l: any) =>
           getChartsOption(l.WeatherElement)
         );
-        setData(json);
+        const manipulatedData = getTableData(json);
+        setData(manipulatedData);
+        setColumns(getTableColumns(json.Location[0].WeatherElement[0]));
         setDistIndex(0);
       });
   }, [location]);
@@ -79,20 +170,72 @@ export default function WeeklyWeather() {
             <option key={l}>{l}</option>
           ))}
         </select>
-        {dists && (
-          <select
-            className="text-xl"
-            onChange={(e) => setDistIndex(parseInt(e.target.value))}
-          >
-            {dists.map((d: { value: string; label: string }, i: number) => (
-              <option key={i} value={i}>
-                {d.label}
-              </option>
-            ))}
-          </select>
-        )}
       </div>
-      {data && <Chart options={data.chart[distIndex]} />}
+      {data && columns && (
+        <div className="overflow-x-auto relative border border-gray-300 mb-6">
+          <table>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header, i) => (
+                    <th
+                      key={header.id}
+                      className={`border border-gray-200 ${
+                        i === 0
+                          ? "sticky bg-gray-100 w-max left-[-1px] border-l border-gray-200"
+                          : "p-2"
+                      }`}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows?.map((row) => (
+                <tr key={row.id} className="hover:bg-slate-100">
+                  {row.getVisibleCells().map((cell, i) => (
+                    <td
+                      key={cell.id}
+                      className={`border border-gray-20 p-2 ${
+                        i === 0 ? "sticky bg-gray-100 w-max left-[-1px]" : ""
+                      }`}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {data && (
+        <div>
+          {dists && (
+            <select
+              className="text-xl"
+              onChange={(e) => setDistIndex(parseInt(e.target.value))}
+            >
+              {dists.map((d: { value: string; label: string }, i: number) => (
+                <option key={i} value={i}>
+                  {d.label}
+                </option>
+              ))}
+            </select>
+          )}
+          <Chart options={data.chart[distIndex]} />
+        </div>
+      )}
     </div>
   );
 }
